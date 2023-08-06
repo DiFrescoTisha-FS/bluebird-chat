adea32c6347a302b5aaafa21ee44177cfd9b8a0b
Tisha Di Fresco <tbdifresco@student.fullsail.edu>
2023-08-06 12:52:27 -0400

    finished sidebar

---
+++ src/components/Cat.js
@@ -0,0 +1,10 @@
+import { Avatar } from '@mui/material'
+import React from 'react'
+
+const Cat = () => {
+  return (
+    <Avatar src="https://images.unsplash.com/photo-1580489944761-15a19d654956?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=761&q=80" />
+  )
+}
+
+export default Cat
---
+++ src/pages/dashboard/GeneralApp.js
@@ -0,0 +1,16 @@
+import { Suspense, lazy } from "react";
+
+const Cat = lazy(() => import("../../components/Cat"));
+
+const GeneralApp = () => {
+
+  return (
+    <>
+      <Suspense fallback="Loading...">
+        <Cat />
+      </Suspense>
+    </>
+  );
+};
+
+export default GeneralApp;
